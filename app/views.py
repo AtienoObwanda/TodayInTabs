3c017c51d977334bcf05d750ebd92a2dd84d0dbd
atienoobwanda <atienoobwanda@gmail.com>
2022-04-29 09:16:18 +0300

    Adding routes and news page

--- app/views.py
+++ app/views.py
@@ -8,4 +8,11 @@ def index():
     View root page function for returning index page information
     '''
     return render_template('index.html')
-    
+
+
+@app.route('/news/<news_id>')
+def news(news_id):
+    '''
+    returns the news page
+    '''
+    return render_template('news.html', id=news_id)
