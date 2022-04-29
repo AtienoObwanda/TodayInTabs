7858f86a6b759649f4d760a7851e7031f91ce7a7
atienoobwanda <atienoobwanda@gmail.com>
2022-04-29 09:22:16 +0300

    setting up config in innit

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 #initialize app
 app = Flask(__name__)
+
+#setting up configuration:
+app.config.from_object =(DevConfig)
+
 
 from app import views
