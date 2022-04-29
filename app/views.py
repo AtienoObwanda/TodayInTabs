4f044f0facce6d3a8717e545e32b0d30dca957f5
atienoobwanda <atienoobwanda@gmail.com>
2022-04-30 02:51:18 +0300

    Initializing requests in views

--- app/views.py
+++ app/views.py
@@ -1,14 +1,23 @@
 from flask import render_template
 from app import app
-
+from .request import get_sources
 # creating views
 @app.route('/')
 def index():
     '''
     View root page function for returning index page information
     '''
+    # Fetching categories ie sports, business and technology:
+
+    technologySource = get_sources('technology')
+    businessSource = get_sources('business')
+    sportSource = get_sources('sports')
+
+
     title = 'Today In Tabs- Stay at the top of the headlines...'
-    return render_template('index.html',title=title)
+
+
+    return render_template('index.html',title=title, technologySource =technologySource, businessSource = businessSource, sportSource = sportSource)
 
 
 @app.route('/articles/<id>')
