5501c56eae8b9660c987b1da9b7891085a522c58
atienoobwanda <atienoobwanda@gmail.com>
2022-05-01 22:04:49 +0300

    styling display cards

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class Article:
         self.title = title
         self.description = description
         self.url = url
-        self.image = 'https:/' +image
+        self.image = image
         self.publishedAt = publishedAt
         self.content = content
 
--- app/views.py
+++ app/views.py
@@ -2,8 +2,24 @@ from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_sources, get_articles,get_article, get_source
 # creating views
+
 @app.route('/')
 def index():
+    '''
+    Display the top sources for different categories
+    '''
+# Fetching sources
+    technology_sources = get_sources('technology')
+    business_sources = get_sources('business')
+    sports_sources = get_sources('sports')
+    entertainment_sources = get_sources('entertainment')
+    title = 'Today In Tabs- Top sources'
+
+    return render_template('index.html',title=title,business=business_sources,technology=technology_sources,
+    sports=sports_sources,entertainment=entertainment_sources )
+
+@app.route('/articles/')
+def articles():
     '''
     View root page function for returning index page information
     '''
@@ -16,24 +32,10 @@ def index():
 
     title = 'Today In Tabs- Stay at the top of the headlines...'
 
-    return render_template('index.html',title=title, technology = technology_articles, 
+    return render_template('articles.html',title=title, technology = technology_articles, 
     business = business_articles, sports = sports_articles, 
     entertainment=entertainment_articles, us=top_sources)
 
-@app.route('/sources/')
-def sources():
-    '''
-    Display the top sources for different categories
-    '''
-# Fetching sources
-    technology_sources = get_sources('technology')
-    business_sources = get_sources('business')
-    sports_sources = get_sources('sports')
-    entertainment_sources = get_sources('entertainment')
-    title = 'Today In Tabs- Top sources'
-
-    return render_template('sources.html',title=title,business=business_sources,technology=technology_sources,
-    sports=sports_sources,entertainment=entertainment_sources )
 
 @app.route('/sources/<id>')
 def source(id):
