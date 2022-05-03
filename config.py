d6bcd5077c129f56943b92282bea3b81cc9a9093
atienoobwanda <atienoobwanda@gmail.com>
2022-05-03 12:08:11 +0300

    Updated url

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
-    ARTICLE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    ARTICLE_BASE_URL='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    # ARTICLE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
     ARTICLES_URL='https://newsapi.org/v2/top-headlines?language=en&category={}&apiKey={}'
     SOURCE_API_URL='https://newsapi.org/v2/top-headlines/sources?category={}&language=en&apiKey={}'
    
